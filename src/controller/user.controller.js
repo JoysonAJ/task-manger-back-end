fbb092dca579dff33c19db2ae1f8eff17c2b3e7f
JoysonAJ <joysondsouzaaj@gmail.com>
2026-01-29 22:19:42 +0530

    Feat - logout

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -69,3 +69,20 @@ export const login = async (req, res) => {
     message: "Login successful",
   });
 };
+
+
+/**
+ * Logs out the current user
+ * @route POST /api/users/logout
+ */
+export const logout = async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "strict",
+  });
+
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
+};
