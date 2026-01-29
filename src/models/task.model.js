b105ea6f9d54a5c5f7e0efc94b980f8f77c9443d
JoysonAJ <joysondsouzaaj@gmail.com>
2026-01-29 23:30:49 +0530

    model completed

---
+++ src/models/task.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+  
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+  
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
+      },
+    },
+    { timestamps: true }
+  );
+  
+  export const Task = mongoose.model("Task", taskSchema);
