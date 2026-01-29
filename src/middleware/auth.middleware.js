edee7c43591dac1fd3ff3c6700b2fcd6c359941e
JoysonAJ <joysondsouzaaj@gmail.com>
2026-01-29 22:20:39 +0530

    Feat - logout

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+// Middleware to protect routes ,
+
+export const protect = async (req, res, next) => {
+    try {
+        const token = req.cookies?.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized, token missing" });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User no longer exists" });
+    }
+
+    req.user = user; // attach authenticated user to request
+    next();
+    } catch (error) {
+        return res.status(401).json({ message: "Not authorized, invalid token" });
+    }
+}
