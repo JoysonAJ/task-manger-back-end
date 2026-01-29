0a9735adceb174f2915a6510b353be1bfaf28ff9
JoysonAJ <joysondsouzaaj@gmail.com>
2026-01-29 23:22:58 +0530

    route - added logout rout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login,register} from "../controller/user.controller.js"
+import { login,register,logout} from "../controller/user.controller.js"
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout",protect, logout);
 
 export default router;
